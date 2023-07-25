ffc61512bd0c98045d631f3b03303a9b7ff11022
Senadheera330 <101629162+Senadheera330@users.noreply.github.com>
2023-07-25 13:20:07 +0530

    Furniro

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,11 @@
+import React from "react";
 import "../styles/navbar.css";
 import Logo from "../images/logo.png";
 import { Link } from "react-router-dom";
+import cart from '../images/icons/cart.png';
+import wishlist from '../images/icons/whishlist.png';
+import search from '../images/icons/search.png';
+import userProfile from '../images/icons/user.png';
 
 function Navbar() {
     return (
@@ -10,7 +15,7 @@ function Navbar() {
             <img src={Logo} className="nav_logo_img"/>
         furniro
         </Link>
-        <ul>
+        <ul className="navbar_page_links">
         <li>
             <Link to="/">Home</Link>
         </li>
@@ -23,6 +28,21 @@ function Navbar() {
         <li>
             <Link to="/contact">contact</Link>
         </li>
+        </ul>
+
+        <ul>
+            <li>
+                <Link to=""><img src={userProfile} className="navbar_icon" /></Link>
+            </li>
+            <li>
+                <Link to=""><img src={search} className="navbar_icon"/></Link>
+            </li>
+            <li>
+                <Link to=""><img src={wishlist} className="navbar_icon"/></Link>
+            </li>
+            <li>
+                <Link to=""><img src={cart} className="navbar_icon"/></Link>
+            </li>
         </ul>
     </div>
     );
